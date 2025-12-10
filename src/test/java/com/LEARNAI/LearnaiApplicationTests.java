3526b6bb1bf849801608f32ed9a371d10f95952e
thrishaaaa <thrishakiran14@gmail.com>
2025-12-11 01:18:46 +0530

    first commit

---
+++ src/test/java/com/LEARNAI/LearnaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LEARNAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
