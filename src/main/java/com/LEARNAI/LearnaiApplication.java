82fc5ab0f95b636974f78218bc82b35f9a25e995
thrishaaaa <thrishakiran14@gmail.com>
2025-12-13 00:02:53 +0530

    first commit

--- src/main/java/com/LEARNAI/LearnaiApplication.java
+++ src/main/java/com/LEARNAI/LearnaiApplication.java
@@ -2,8 +2,9 @@ package com.LEARNAI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class LearnaiApplication {
 
 	public static void main(String[] args) {
